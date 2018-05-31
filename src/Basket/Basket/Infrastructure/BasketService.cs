d713f83790d3b329438ea3eb6d4b3f97b8b40615
pierreHenryB <pierrehenry.bartholome@gmail.com>
2018-05-31 12:24:34 +0200

    Avancement dans le tp

---
+++ src/Basket/Basket/Infrastructure/BasketService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.Infrastructure
+{
+    public class BasketService
+    {
+        public Domain.Basket GetBasket(IList<BasketLineArticle> basketLineArticles)
+        {
+            // here your code implementation
+        }   
+    }
+}
